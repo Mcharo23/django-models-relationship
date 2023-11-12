ede2d64750eb414edabe129c85373a2204758dda
Mcharo23 <mcharoprofg23@gmail.com>
2023-11-12 11:44:17 +0300

    changing url name

--- products/urls.py
+++ products/urls.py
@@ -4,15 +4,15 @@ from .views import ProductViewset
 urlpatterns = [
     path('', ProductViewset.as_view({'get': 'list'}), name='product-list'),
     path('<int:pk>/',
-         ProductViewset.as_view({'get': 'retrieve'}), name='product-retrieve'),
+         ProductViewset.as_view({'get': 'retrieve'}), name='id'),
     path(
-        'create-product/', ProductViewset.as_view({'post': 'create_product'}), name='product-create'),
+        'create-product/', ProductViewset.as_view({'post': 'create_product'}), name='create-product'),
     path(
-        'create-category/', ProductViewset.as_view({'post': 'create_category'}), name='category-create'),
+        'create-category/', ProductViewset.as_view({'post': 'create_category'}), name='create-product'),
     path(
-        'create-brand/', ProductViewset.as_view({'post': 'create_brand'}), name='brand-create'),
+        'create-brand/', ProductViewset.as_view({'post': 'create_brand'}), name='create-brand'),
     path(
         '<int:pk>/place-order/', ProductViewset.as_view({'post': 'order'}), name='place-order'),
     path(
-        '<int:pk>/delete-product/', ProductViewset.as_view({'post': 'create_brand'}), name='brand-create'),
+        '<int:pk>/delete-order/', ProductViewset.as_view({'post': 'create_brand'}), name='delete-order'),
 ]
